b6f799711c38b45a6be18350862a16ecaea36916
zzzzDenzzzz <12den24@gmail.com>
2022-12-14 00:22:35 +0300

    Тестирование работы модели-представления

--- ViewModels/Base/ViewModel.cs
+++ ViewModels/Base/ViewModel.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace CV19.ViewModels.Base
 {
-    internal class ViewModel
+    internal abstract class ViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (Equals(field, value))
+            {
+                return false;
+            }
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
     }
 }
---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,26 @@
+﻿using CV19.ViewModels.Base;
+
+namespace CV19.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        string _title = "Анализ статистики CV19";
+
+        /// <summary>заголовок окна</summary>
+        public string Title
+        {
+            get => _title;
+            //set
+            //{
+            //    //if (Equals(_title, value))
+            //    //{
+            //    //    return;
+            //    //}
+            //    //_title = value;
+            //    //OnPropertyChanged();
+            //    Set(ref _title, value);
+            //}
+            set => Set(ref _title, value);
+        }
+    }
+}
