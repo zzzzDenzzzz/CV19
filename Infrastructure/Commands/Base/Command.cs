984eab92d5856e25ba85e7784eb62804f6bfc345
zzzzDenzzzz <12den24@gmail.com>
2022-12-14 10:56:31 +0300

    Базовый класс команды

--- Infrastructure/Commands/Base/Command.cs
+++ Infrastructure/Commands/Base/Command.cs
@@ -1,12 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace CV19.Infrastructure.Commands.Base
 {
-    internal class Command
+    internal abstract class Command : ICommand
     {
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public abstract bool CanExecute(object parameter);
+
+        public abstract void Execute(object parameter);
     }
 }
