c5c473f95a4d56f6cc526af15f28515e138eb2a0
zzzzDenzzzz <12den24@gmail.com>
2022-12-17 11:08:13 +0300

    Тестовая предметная область

---
+++ Models/Decanat/Student.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CV19.Models.Decanat
+{
+    internal class Student
+    {
+    }
+}
